2851e45398b01fae2e2d5fcffdfccf9771e5d2ca
KIMEUNJI <109258160+ej9374@users.noreply.github.com>
2026-05-24 00:11:16 +0900

    chore: 프로젝트 초기 세팅 설정

---
+++ src/main/kotlin/teamyg/parfait/ParfaitApplication.kt
@@ -0,0 +1,11 @@
+package teamyg.parfait
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParfaitApplication
+
+fun main(args: Array<String>) {
+	runApplication<ParfaitApplication>(*args)
+}
---
+++ src/test/kotlin/teamyg/parfait/ParfaitApplicationTests.kt
@@ -0,0 +1,13 @@
+package teamyg.parfait
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParfaitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
