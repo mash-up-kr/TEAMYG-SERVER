593a1bd47be5d11eae23451eb320f3056edb4cce
KIMEUNJI <109258160+ej9374@users.noreply.github.com>
2026-05-27 23:58:25 +0900

    [Chore]: ktlint 설정 추가
    * chore: ktlint 설정 추가 및 코드 포맷 적용
    
    * chore: .gitignore에 Claude Code 관련 파일 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("plugin.jpa") version "2.2.21" apply false
     id("org.springframework.boot") version "4.0.6" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
 }
 
 allprojects {
@@ -12,6 +13,12 @@ allprojects {
 }
 
 subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("1.5.0")
+    }
+
     repositories {
         mavenCentral()
     }
